ad190714988f5dea7e70116f4565df2fb362fe1a
cedric-bour <cedric.bour@live.fr>
2020-01-29 09:58:39 +0000

    update

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,8 @@ if [ $# -eq 0 ]; then
     echo "apply/delete name replicat"
     exit 1
 fi
-cat << EOF | kubectl $1 -f -
+
+script="$(cat << EOF
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -22,4 +23,18 @@ spec:
       containers:
       - name: $2
         image: localhost:5000/$2
+        
 EOF
+)"
+
+if [ $2 == "openvpn" ]; then
+script+="$(cat << EOF
+securityContext:
+          capabilities:
+            add:
+              - NET_ADMIN
+EOF
+)"
+fi
+
+echo "$script" | kubectl $1 -f -
--- service.sh
+++ service.sh
@@ -1,17 +1,47 @@
 if [ $# -eq 0 ]; then
-    echo "apply/delete name port containerport"
+    echo "apply/delete name port exposed:false"
     exit 1
 fi
-cat << EOF | kubectl $1 -f -
+
+script="$(cat << EOF
 apiVersion: v1
 kind: Service
 metadata:
   name: backend-$2
 spec:
   ports:
-  - port: $3
-    targetPort: $4
-  selector:
+  
+EOF
+)"
+
+
+listports=$(echo $3 | tr ";" "\n")
+for ports in $listports
+do
+IFS=', ' read -r -a port <<< "$ports"
+script+="$(cat << EOF
+- name: "${port[0]}"
+    port: ${port[0]}
+    targetPort: ${port[1]}
+  
+EOF
+)"
+done
+
+if [ $4 == "true" ]; then
+script+="$(cat << EOF
+externalIPs:
+    - 91.121.82.115
+  
+EOF
+)"
+fi
+
+script+="$(cat << EOF
+selector:
     app: $2
     tier: $2
 EOF
+)"
+
+echo "$script" | kubectl $1 -f -
